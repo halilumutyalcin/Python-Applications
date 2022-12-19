f31fc4d61a144485d74b01c7e9bd7b190b775db5
Halil Umut Yalçın <72702654+halilumutyalcin@users.noreply.github.com>
2022-12-19 17:58:58 +0300

    19.12.2022
    I reviewed my codes and added new things.

---
+++ Decoder and Encoder/decoder.py
@@ -0,0 +1,19 @@
+def encrypt(text):
+    pswd = ""
+    for k in text:
+        print(k, "=>", chr(ord(k) + 5))
+        pswd = pswd + chr(ord(k) + 5)
+    print(text, " = >", pswd)
+
+def decrypt(pswd):
+    text = ""
+    for k in pswd:
+        print(k, "=>", chr(ord(k) - 5))
+        text = text + chr(ord(k) - 5)
+    print(pswd, " = >", text)
+
+text = input("Enter the text to be encrypted:")
+encrypt(text)
+
+pswd = input("Enter the text to be decrypted:")
+decrypt(pswd)
